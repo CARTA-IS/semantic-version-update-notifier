3ae46968fddcfa4f5f64d59489090f3598afe39f
GilYoung Song <gysong@carta.is>
2022-01-20 20:54:19 +0900

    sementic version update notifier (#7)
    * fix: remove template code
    
    * dependency: github, octockit, semver installed
    
    * chore: remove unused var
    
    * chore: force type cast
    
    * chore: lint fix
    
    * dependency: semver-type
    
    * chore: remove unused var
    
    * chore: fix typescript-lint
    
    * chore: disst lint update
    
    * settings: test off
    
    * chore: core.debug to core.info
    
    * chore: update dist
    
    * chore: change actions/core import

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,53 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/rest'
+import semver from 'semver'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+const {GITHUB_REPOSITORY, PERSONAL_TOKEN} = process.env
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const [owner, repoName] = (GITHUB_REPOSITORY as string).split('/')
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+async function run(): Promise<undefined> {
+  const octokit = new Octokit({auth: PERSONAL_TOKEN})
+  core.info(`[----${owner}/${repoName}----]`)
+  const releases = await octokit.paginate(octokit.rest.repos.listReleases, {
+    owner,
+    repo: repoName
+  })
+  core.info(`Found ${releases.length} releases`)
+  if (releases.length < 2) {
+    return
   }
+
+  const lastRelease = releases[0]
+  const secondLastRelease = releases[1]
+  core.info(lastRelease.tag_name)
+  core.info(secondLastRelease.tag_name)
+
+  const lastReleaseMajor = semver.major(lastRelease.tag_name)
+  const lastReleaseMinor = semver.minor(lastRelease.tag_name)
+
+  const secondLastReleaseMajor = semver.major(secondLastRelease.tag_name)
+  const secondLastReleaseMinor = semver.minor(secondLastRelease.tag_name)
+
+  let isMajorUpdated = false
+  let isMinorUpdated = false
+  let semanticTarget = ''
+  if (lastReleaseMajor > secondLastReleaseMajor) {
+    semanticTarget = '주'
+    isMajorUpdated = true
+  } else if (lastReleaseMinor > secondLastReleaseMinor) {
+    semanticTarget = '부'
+    isMinorUpdated = true
+  } else {
+    return
+  }
+
+  core.info(`isMajorUpdated: ${isMajorUpdated}`)
+  core.info(`isMinorUpdated: ${isMinorUpdated}`)
+
+  const message = `${repoName} 의 **${semanticTarget}** 버전이 올라갔습니다 (${secondLastRelease.tag_name} -> ${lastRelease.tag_name})`
+  core.info(message)
+  core.setOutput('message', message)
 }
 
 run()
