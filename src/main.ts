2577ede0b16a8dfac56bc92fec7ee8c36bf4483d
Ashe <tech@ashe.kr>
2022-01-20 20:02:35 +0900

    fix: remove template code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,59 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { Octokit } from "@octokit/rest";
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+const core = require('@actions/core');
+const github = require('@actions/github');
+const semver = require('semver');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const {
+  GITHUB_REPOSITORY,
+  PERSONAL_TOKEN,
+  GITHUB_REF,
+} = process.env;
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+const [owner, repoName] = GITHUB_REPOSITORY.split('/');
+
+async function run() {
+  const octokit = await new Octokit({auth: PERSONAL_TOKEN})
+  console.log(`[----${owner}/${repoName}----]`)
+  let releases = await octokit.paginate(
+    octokit.rest.repos.listReleases,
+    {owner: owner, repo: repoName}
+  )
+  console.log(`Found ${releases.length} releases`)
+  if (releases.length < 2) {
+    return
   }
+
+  const lastRelease = releases[0]
+  const secondLastRelease = releases[1]
+  console.log(lastRelease.tag_name)
+  console.log(secondLastRelease.tag_name)
+
+  const lastReleaseMajor = semver.major(lastRelease.tag_name)
+  const lastReleaseMinor = semver.minor(lastRelease.tag_name)
+
+  const secondLastReleaseMajor = semver.major(secondLastRelease.tag_name)
+  const secondLastReleaseMinor = semver.minor(secondLastRelease.tag_name)
+
+  let isMajorUpdated = false;
+  let isMinorUpdated = false;
+  let semanticTarget = ''
+  if (lastReleaseMajor > secondLastReleaseMajor)  {
+    semanticTarget = '주'
+    isMajorUpdated = true;
+  } else if (lastReleaseMinor > secondLastReleaseMinor) {
+    semanticTarget = '부'
+    isMinorUpdated = true;
+  } else {
+    return
+  }
+
+  console.log(`isMajorUpdated: ${isMajorUpdated}`)
+  console.log(`isMinorUpdated: ${isMinorUpdated}`)
+
+  const message = `${repoName} 의 **${semanticTarget}** 버전이 올라갔습니다 (${secondLastRelease.tag_name} -> ${lastRelease.tag_name})`
+  console.log(message)
+  core.setOutput('message', message)
 }
 
-run()
+run();
